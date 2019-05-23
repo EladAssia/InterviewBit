dafc1ad7f3c0dc639b3bd84f3c7e7f144be99a5e
EladAssia <assiaelad@gmail.com>
2019-05-23 17:59:37 +0300

    Update Repeat and_Missing_Number_Array.py

--- Arrays/Repeat and_Missing_Number_Array.py
+++ Arrays/Repeat and_Missing_Number_Array.py
@@ -1,20 +1,20 @@
-You are given a read only array of n integers from 1 to n.
+# You are given a read only array of n integers from 1 to n.
 
-Each integer appears exactly once except A which appears twice and B which is missing.
+# Each integer appears exactly once except A which appears twice and B which is missing.
 
-Return A and B.
+# Return A and B.
 
-Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
-Note that in your output A should precede B.
+# Note that in your output A should precede B.
 
-Example:
+# Example:
 
-Input:[3 1 2 5 3] 
+# Input:[3 1 2 5 3] 
 
-Output:[3, 4] 
+# Output:[3, 4] 
 
-A = 3, B = 4
+# A = 3, B = 4
 
 ##########################################################################################################################################
 
