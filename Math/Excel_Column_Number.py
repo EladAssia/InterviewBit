4268433bb6a5428b42354e35ee4dfb960c36fd74
EladAssia <assiaelad@gmail.com>
2019-05-23 18:37:14 +0300

    Update Excel_Column_Number.py

--- Math/Excel_Column_Number.py
+++ Math/Excel_Column_Number.py
@@ -1,20 +1,20 @@
-Given a column title as appears in an Excel sheet, return its corresponding column number.
+# Given a column title as appears in an Excel sheet, return its corresponding column number.
 
-Example:
+# Example:
 
-    A -> 1
+#     A -> 1
     
-    B -> 2
+#     B -> 2
     
-    C -> 3
+#     C -> 3
     
-    ...
+#     ...
     
-    Z -> 26
+#     Z -> 26
     
-    AA -> 27
+#     AA -> 27
     
-    AB -> 28 
+#     AB -> 28 
     
 ##########################################################################################################################################
 
