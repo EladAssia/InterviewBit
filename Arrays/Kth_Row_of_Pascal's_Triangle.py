f5a371515093b572d20b68175c0273dd9e3d9216
EladAssia <assiaelad@gmail.com>
2019-05-23 18:06:57 +0300

    Create Kth_Row_of_Pascal's_Triangle.py

---
+++ Arrays/Kth_Row_of_Pascal's_Triangle.py
@@ -0,0 +1,35 @@
+Given an index k, return the kth row of the Pascal’s triangle.
+
+Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
+
+Example:
+
+Input : k = 3
+
+Return : [1,3,3,1]
+
+NOTE: k is 0 based. k = 0, corresponds to the row [1]. 
+Note: Could you optimize your algorithm to use only O(k) extra space?
+
+##########################################################################################################################################
+
+class Solution:
+    # @param A : integer
+    # @return a list of integers
+    def getRow(self, A):
+        if A == 0:
+            return [1]
+        
+        my_list = []
+        for ii in range(0, A+1):
+            tmp_list = []
+            for jj in range(0, ii+1):
+                if jj == 0 or jj == ii:
+                    tmp_list.append(1)
+                else:
+                    tmp_list.append(my_list[ii-1][jj-1]+my_list[ii-1][jj])
+            my_list.append(tmp_list)
+        
+        return tmp_list
+
+##########################################################################################################################################
