5bc6fd9a831a86b7e7e3c9da4fc75190fc4c872a
EladAssia <assiaelad@gmail.com>
2019-05-23 18:23:36 +0300

    Update Rotate_Matrix.py

--- Arrays/Rotate_Matrix.py
+++ Arrays/Rotate_Matrix.py
@@ -1,25 +1,25 @@
-You are given an n x n 2D matrix representing an image.
+# You are given an n x n 2D matrix representing an image.
 
-Rotate the image by 90 degrees (clockwise).
+# Rotate the image by 90 degrees (clockwise).
 
-You need to do this in place.
+# You need to do this in place.
 
-Note that if you end up using an additional array, you will only receive partial score.
+# Note that if you end up using an additional array, you will only receive partial score.
 
-Example:
+# Example:
 
-If the array is
+# If the array is
 
-[
-    [1, 2],
-    [3, 4]
-]
-Then the rotated array becomes:
+# [
+#     [1, 2],
+#     [3, 4]
+# ]
+# Then the rotated array becomes:
 
-[
-    [3, 1],
-    [4, 2]
-]
+# [
+#     [3, 1],
+#     [4, 2]
+# ]
 
 ##########################################################################################################################################
 
