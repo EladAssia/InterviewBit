971234358305be064799e6901f7d2a956c0f308b
EladAssia <assiaelad@gmail.com>
2019-05-23 18:36:57 +0300

    Create Excel_Column_Number.py

---
+++ Math/Excel_Column_Number.py
@@ -0,0 +1,30 @@
+Given a column title as appears in an Excel sheet, return its corresponding column number.
+
+Example:
+
+    A -> 1
+    
+    B -> 2
+    
+    C -> 3
+    
+    ...
+    
+    Z -> 26
+    
+    AA -> 27
+    
+    AB -> 28 
+    
+##########################################################################################################################################
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def titleToNumber(self, A):
+        col_num = 0
+        for ii in range(len(A)):
+            col_num += (ord(A[ii])-64)*26**(len(A)-ii-1)
+        return col_num
+
+##########################################################################################################################################
