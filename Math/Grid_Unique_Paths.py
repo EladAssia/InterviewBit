179a0bc8e50828eec259cbb13e49fe032f6ed15b
EladAssia <assiaelad@gmail.com>
2019-05-23 18:43:19 +0300

    Update Grid_Unique_Paths.py

--- Math/Grid_Unique_Paths.py
+++ Math/Grid_Unique_Paths.py
@@ -1,18 +1,18 @@
-A robot is located at the top-left corner of an A x B grid.
+# A robot is located at the top-left corner of an A x B grid.
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked ‘Finish’ in the diagram below).
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked ‘Finish’ in the diagram below).
 
-How many possible unique paths are there?
+# How many possible unique paths are there?
 
-Note: A and B will be such that the resulting answer fits in a 32 bit signed integer.
+# Note: A and B will be such that the resulting answer fits in a 32 bit signed integer.
 
-Example :
+# Example :
 
-Input : A = 2, B = 2
-Output : 2
+# Input : A = 2, B = 2
+# Output : 2
 
-2 possible routes : (0, 0) -> (0, 1) -> (1, 1) 
-              OR  : (0, 0) -> (1, 0) -> (1, 1)
+# 2 possible routes : (0, 0) -> (0, 1) -> (1, 1) 
+#               OR  : (0, 0) -> (1, 0) -> (1, 1)
               
 ##########################################################################################################################################
 
