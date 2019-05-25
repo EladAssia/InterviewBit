ba0adaef31167d4a9c05270249d917d4fa65326d
EladAssia <assiaelad@gmail.com>
2019-05-25 18:43:17 +0300

    Update Length_of_Last_Word.py

--- Strings/Length_of_Last_Word.py
+++ Strings/Length_of_Last_Word.py
@@ -1,16 +1,16 @@
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
 
-If the last word does not exist, return 0.
+# If the last word does not exist, return 0.
 
-Note: A word is defined as a character sequence consists of non-space characters only.
+# Note: A word is defined as a character sequence consists of non-space characters only.
 
-Example:
+# Example:
 
-Given s = "Hello World",
+# Given s = "Hello World",
 
-return 5 as length("World") = 5.
+# return 5 as length("World") = 5.
 
-Please make sure you try to solve this problem without using library functions. Make sure you only traverse the string once.
+# Please make sure you try to solve this problem without using library functions. Make sure you only traverse the string once.
 
 ##########################################################################################################################################
 
