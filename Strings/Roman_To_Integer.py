007c8f8e13d02cd5946b824f9a1286b241da34be
EladAssia <assiaelad@gmail.com>
2019-05-25 18:45:19 +0300

    Update Roman_To_Integer.py

--- Strings/Roman_To_Integer.py
+++ Strings/Roman_To_Integer.py
@@ -1,13 +1,13 @@
-Given a roman numeral, convert it to an integer.
+# Given a roman numeral, convert it to an integer.
 
-Input is guaranteed to be within the range from 1 to 3999.
+# Input is guaranteed to be within the range from 1 to 3999.
 
-Example :
+# Example :
 
-Input : "XIV"
-Return : 14
-Input : "XX"
-Output : 20
+# Input : "XIV"
+# Return : 14
+# Input : "XX"
+# Output : 20
 
 ##########################################################################################################################################
 
