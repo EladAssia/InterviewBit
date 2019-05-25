a48b1b42258a8fcca910a0a789a63d2dbc93eb0e
EladAssia <assiaelad@gmail.com>
2019-05-25 18:19:30 +0300

    Update Implement_Power_Function.py

--- Binary Search/Implement_Power_Function.py
+++ Binary Search/Implement_Power_Function.py
@@ -1,16 +1,16 @@
-Implement pow(x, n) % d.
+# Implement pow(x, n) % d.
 
-In other words, given x, n and d,
+# In other words, given x, n and d,
 
-find (xn % d)
+# find (xn % d)
 
-Note that remainders on division cannot be negative. 
-In other words, make sure the answer you return is non negative.
+# Note that remainders on division cannot be negative. 
+# In other words, make sure the answer you return is non negative.
 
-Input : x = 2, n = 3, d = 3
-Output : 2
+# Input : x = 2, n = 3, d = 3
+# Output : 2
 
-2^3 % 3 = 8 % 3 = 2.
+# 2^3 % 3 = 8 % 3 = 2.
 
 ##########################################################################################################################################
 
