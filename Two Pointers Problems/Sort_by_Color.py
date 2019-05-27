f5bc573fb6cbf3779bc537007aa16fe5ae7b3916
EladAssia <assiaelad@gmail.com>
2019-05-27 16:51:53 +0300

    Update Sort_by_Color.py

--- Two Pointers Problems/Sort_by_Color.py
+++ Two Pointers Problems/Sort_by_Color.py
@@ -1,14 +1,14 @@
-Given an array with n objects colored red, white or blue, 
-sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+# Given an array with n objects colored red, white or blue, 
+# sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 
-Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+# Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 
-Note: Using library sort function is not allowed.
+# Note: Using library sort function is not allowed.
 
-Example :
+# Example :
 
-Input : [0 1 2 0 1 2]
-Modify array so that it becomes : [0 0 1 1 2 2]
+# Input : [0 1 2 0 1 2]
+# Modify array so that it becomes : [0 0 1 1 2 2]
 
 ##########################################################################################################################################
 
