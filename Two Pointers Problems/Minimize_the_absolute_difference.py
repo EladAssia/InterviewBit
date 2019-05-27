d04cd876ac285fb67a8079a4c523293fd00ef47f
EladAssia <assiaelad@gmail.com>
2019-05-27 16:50:48 +0300

    Update Minimize_the_absolute_difference.py

--- Two Pointers Problems/Minimize_the_absolute_difference.py
+++ Two Pointers Problems/Minimize_the_absolute_difference.py
@@ -1,20 +1,20 @@
-Given three sorted arrays A, B and Cof not necessarily same sizes.
+# Given three sorted arrays A, B and Cof not necessarily same sizes.
 
-Calculate the minimum absolute difference between the maximum and minimum number from the triplet a, b, c such that a, b, c 
-belongs arrays A, B, C respectively.
-i.e. minimize | max(a,b,c) - min(a,b,c) |.
+# Calculate the minimum absolute difference between the maximum and minimum number from the triplet a, b, c such that a, b, c 
+# belongs arrays A, B, C respectively.
+# i.e. minimize | max(a,b,c) - min(a,b,c) |.
 
-Example :
+# Example :
 
-Input:
+# Input:
 
-A : [ 1, 4, 5, 8, 10 ]
-B : [ 6, 9, 15 ]
-C : [ 2, 3, 6, 6 ]
-Output:
+# A : [ 1, 4, 5, 8, 10 ]
+# B : [ 6, 9, 15 ]
+# C : [ 2, 3, 6, 6 ]
+# Output:
+# 1
 
-1
-Explanation: We get the minimum difference for a=5, b=6, c=6 as | max(a,b,c) - min(a,b,c) | = |6-5| = 1.
+# Explanation: We get the minimum difference for a=5, b=6, c=6 as | max(a,b,c) - min(a,b,c) | = |6-5| = 1.
 
 ##########################################################################################################################################
 
