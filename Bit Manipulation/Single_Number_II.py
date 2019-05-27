a14d0b0053b5b6526d313666317498504129f346
EladAssia <assiaelad@gmail.com>
2019-05-27 16:35:07 +0300

    Update Single_Number_II.py

--- Bit Manipulation/Single_Number_II.py
+++ Bit Manipulation/Single_Number_II.py
@@ -1,15 +1,15 @@
-Given an array of integers, every element appears thrice except for one which occurs once.
+# Given an array of integers, every element appears thrice except for one which occurs once.
 
-Find that element which does not appear thrice.
+# Find that element which does not appear thrice.
 
-Note: Your algorithm should have a linear runtime complexity.
+# Note: Your algorithm should have a linear runtime complexity.
 
-Could you implement it without using extra memory?
+# Could you implement it without using extra memory?
 
-Example :
+# Example :
 
-Input : [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
-Output : 4
+# Input : [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+# Output : 4
 
 ##########################################################################################################################################
 
