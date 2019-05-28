8b506064e92454264f492e6331d6ba470ae85359
EladAssia <assiaelad@gmail.com>
2019-05-28 13:50:07 +0300

    Update Nearest_Smaller_Element.py

--- Stacks And Queues/Nearest_Smaller_Element.py
+++ Stacks And Queues/Nearest_Smaller_Element.py
@@ -1,23 +1,23 @@
-Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
+# Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
 
-More formally,
+# More formally,
 
-G[i] for an element A[i] = an element A[j] such that 
-    j is maximum possible AND 
-    j < i AND
-    A[j] < A[i]
+# G[i] for an element A[i] = an element A[j] such that 
+#     j is maximum possible AND 
+#     j < i AND
+#     A[j] < A[i]
     
-Elements for which no smaller element exist, consider next smaller element as -1.
+# Elements for which no smaller element exist, consider next smaller element as -1.
 
-Example:
+# Example:
 
-Input : A : [4, 5, 2, 10, 8]
-Return : [-1, 4, -1, 2, 2]
+# Input : A : [4, 5, 2, 10, 8]
+# Return : [-1, 4, -1, 2, 2]
 
-Example 2:
+# Example 2:
 
-Input : A : [3, 2, 1]
-Return : [-1, -1, -1]
+# Input : A : [3, 2, 1]
+# Return : [-1, -1, -1]
 
 ##########################################################################################################################################
 
