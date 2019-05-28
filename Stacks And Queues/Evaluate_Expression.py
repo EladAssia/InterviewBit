48d373633b89a0298a7b1a8cf38cc461890d100a
EladAssia <assiaelad@gmail.com>
2019-05-28 13:48:54 +0300

    Update Evaluate_Expression.py

--- Stacks And Queues/Evaluate_Expression.py
+++ Stacks And Queues/Evaluate_Expression.py
@@ -1,11 +1,11 @@
-Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
 
-Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+# Valid operators are +, -, *, /. Each operand may be an integer or another expression.
 
-Examples:
+# Examples:
 
-["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
-["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+# ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+# ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
   
 ##########################################################################################################################################
 
