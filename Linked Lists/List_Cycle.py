dad72f0b5ab687dae3794589a8526b8fda798eab
EladAssia <assiaelad@gmail.com>
2019-05-28 13:41:39 +0300

    Update List_Cycle.py

--- Linked Lists/List_Cycle.py
+++ Linked Lists/List_Cycle.py
@@ -1,17 +1,17 @@
-Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+# Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
-Try solving it using constant additional space.
+# Try solving it using constant additional space.
 
-Example :
+# Example :
 
-Input : 
+# Input : 
 
-                  ______
-                 |     |
-                 \/    |
-        1 -> 2 -> 3 -> 4
+#                   ______
+#                  |     |
+#                  \/    |
+#         1 -> 2 -> 3 -> 4
 
-Return the node corresponding to node 3. 
+# Return the node corresponding to node 3. 
 
 ##########################################################################################################################################
 
