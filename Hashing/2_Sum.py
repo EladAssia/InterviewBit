ec4dbd6c2f16ad3c18647592a2f4e4fa48c4f025
EladAssia <assiaelad@gmail.com>
2019-05-29 17:01:47 +0300

    Update 2_Sum.py

--- Hashing/2_Sum.py
+++ Hashing/2_Sum.py
@@ -6,7 +6,8 @@
 # (Looking at the function signature will make things clearer). 
 # Note that, if no pair exists, return empty list.
 
-# If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
+# If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, 
+# choose the one with minimum index1 out of them.
 
 # Input: [2, 7, 11, 15], target=9
 # Output: index1 = 1, index2 = 2
