32242aed6a117a9fa1b6328f70c0081fcdac7f99
EladAssia <assiaelad@gmail.com>
2019-05-29 16:26:49 +0300

    Update Permutations.py

--- Backtracking/Permutations.py
+++ Backtracking/Permutations.py
@@ -1,19 +1,20 @@
-Given a collection of numbers, return all possible permutations.
+# Given a collection of numbers, return all possible permutations.
 
-Example:
+# Example:
 
-[1,2,3] will have the following permutations:
+# [1,2,3] will have the following permutations:
 
-[1,2,3]
-[1,3,2]
-[2,1,3] 
-[2,3,1] 
-[3,1,2] 
-[3,2,1]
+# [1,2,3]
+# [1,3,2]
+# [2,1,3] 
+# [2,3,1] 
+# [3,1,2] 
+# [3,2,1]
  
-NOTE
-No two entries in the permutation sequence should be the same.
-For the purpose of this problem, assume that all the numbers in the collection are unique.
+# NOTE:
+# No two entries in the permutation sequence should be the same.
+# For the purpose of this problem, assume that all the numbers in the collection are unique.
+
 ##########################################################################################################################################
 
 def rec_per(A):
