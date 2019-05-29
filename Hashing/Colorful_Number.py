3a3372f27607aa378d300dda007d312b211d2caa
EladAssia <assiaelad@gmail.com>
2019-05-29 16:55:09 +0300

    Update Colorful_Number.py

--- Hashing/Colorful_Number.py
+++ Hashing/Colorful_Number.py
@@ -1,22 +1,22 @@
-For Given Number N find if its COLORFUL number or not
+# For Given Number N find if its COLORFUL number or not
 
-Return 0/1
+# Return 0/1
 
-COLORFUL number:
+# COLORFUL number:
 
-A number can be broken into different contiguous sub-subsequence parts. 
-Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
-And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different
-Example:
+# A number can be broken into different contiguous sub-subsequence parts. 
+# Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
+# And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different
+# Example:
 
-N = 23
-2 3 23
-2 -> 2
-3 -> 3
-23 -> 6
-this number is a COLORFUL number since product of every digit of a sub-sequence are different. 
+# N = 23
+# 2 3 23
+# 2 -> 2
+# 3 -> 3
+# 23 -> 6
+# this number is a COLORFUL number since product of every digit of a sub-sequence are different. 
 
-Output : 1
+# Output : 1
 
 ##########################################################################################################################################
 
