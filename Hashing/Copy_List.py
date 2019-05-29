0e1e95eed6916a4937bb322a2579629fe729aa8e
EladAssia <assiaelad@gmail.com>
2019-05-29 16:55:48 +0300

    Update Copy_List.py

--- Hashing/Copy_List.py
+++ Hashing/Copy_List.py
@@ -1,19 +1,20 @@
-A linked list is given such that each node contains an additional random pointer which could point to any node in the list or NULL.
+# A linked list is given such that each node contains an additional random pointer which could point to any node in the list or NULL.
 
-Return a deep copy of the list.
+# Return a deep copy of the list.
 
-Example
+# Example
 
-Given list
-   1 -> 2 -> 3
+# Given list
+#    1 -> 2 -> 3
 
-with random pointers going from
+# with random pointers going from
 
-  1 -> 3
-  2 -> 1
-  3 -> 1
+#   1 -> 3
+#   2 -> 1
+#   3 -> 1
   
-You should return a deep copy of the list. The returned answer should not contain the same node as the original list, but a copy of them. The pointers in the returned list should not link to any node in the original input list.
+# You should return a deep copy of the list. The returned answer should not contain the same node as the original list, 
+# but a copy of them. The pointers in the returned list should not link to any node in the original input list.
 
 ##########################################################################################################################################
 
