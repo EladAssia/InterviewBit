fdb5399251b1c91076ce35065c444954a67164c9
EladAssia <assiaelad@gmail.com>
2019-05-29 16:40:57 +0300

    Create Longest_Substring_Without_Repeat.py

---
+++ Hashing/Longest_Substring_Without_Repeat.py
@@ -0,0 +1,33 @@
+Given a string, 
+find the length of the longest substring without repeating characters.
+
+Example:
+
+The longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+
+For "bbbbb" the longest substring is "b", with the length of 1.
+
+##########################################################################################################################################
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def lengthOfLongestSubstring(self, A):
+        d, l = {}, 0
+        for ii in range(len(A)):
+            if A[ii] in d:
+                l = max(l, len(d))
+                idx = d[A[ii]]
+                key = list(d.keys())
+                k = 0
+                while k < len(key):
+                    if d[key[k]] <= idx:
+                        del d[key[k]]
+                    k += 1
+                d[A[ii]] = ii
+            else:
+                d[A[ii]] = ii
+        l = max(l, len(d))
+        return l
+        
+##########################################################################################################################################
