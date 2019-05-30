276de2238a9594adf39e227ab98e835bd52272cd
EladAssia <assiaelad@gmail.com>
2019-05-30 10:26:46 +0300

    Update Shortest_Unique_Prefix.py

--- Tree Data Structure/Shortest_Unique_Prefix.py
+++ Tree Data Structure/Shortest_Unique_Prefix.py
@@ -1,15 +1,15 @@
-Find shortest unique prefix to represent each word in the list.
+# Find shortest unique prefix to represent each word in the list.
 
-Example:
+# Example:
 
-Input: [zebra, dog, duck, dove]
-Output: {z, dog, du, dov}
-where we can see that
-zebra = z
-dog = dog
-duck = du
-dove = dov
-NOTE : Assume that no word is prefix of another. In other words, the representation is always possible. 
+# Input: [zebra, dog, duck, dove]
+# Output: {z, dog, du, dov}
+# where we can see that
+# zebra = z
+# dog = dog
+# duck = du
+# dove = dov
+# NOTE : Assume that no word is prefix of another. In other words, the representation is always possible. 
  
 ##########################################################################################################################################
 
