5583aff8af4bbd78cf973b034e560c4fe0544891
EladAssia <assiaelad@gmail.com>
2019-05-30 10:23:57 +0300

    Update Inorder_Traversal_of_Cartesian_Tree.py

--- Tree Data Structure/Inorder_Traversal_of_Cartesian_Tree.py
+++ Tree Data Structure/Inorder_Traversal_of_Cartesian_Tree.py
@@ -1,17 +1,17 @@
-Given an inorder traversal of a cartesian tree, construct the tree.
+# Given an inorder traversal of a cartesian tree, construct the tree.
 
-Cartesian tree : is a heap ordered binary tree, where the root is greater than all the elements in the subtree. 
-Note: You may assume that duplicates do not exist in the tree. 
-Example :
+# Cartesian tree : is a heap ordered binary tree, where the root is greater than all the elements in the subtree. 
+# Note: You may assume that duplicates do not exist in the tree. 
+# Example :
 
-Input : [1 2 3]
+# Input : [1 2 3]
 
-Return :   
-          3
-         /
-        2
-       /
-      1
+# Return :   
+#           3
+#          /
+#         2
+#        /
+#       1
       
 ##########################################################################################################################################
 
