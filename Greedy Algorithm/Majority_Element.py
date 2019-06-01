50f954c9be13400a86bd4fec22ba8076633a92fc
EladAssia <assiaelad@gmail.com>
2019-06-01 12:20:40 +0300

    Update Majority_Element.py

--- Greedy Algorithm/Majority_Element.py
+++ Greedy Algorithm/Majority_Element.py
@@ -1,11 +1,11 @@
-Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+# Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
 
-You may assume that the array is non-empty and the majority element always exist in the array.
+# You may assume that the array is non-empty and the majority element always exist in the array.
 
-Example :
+# Example :
 
-Input : [2, 1, 2]
-Return  : 2 which occurs 2 times which is greater than 3/2. 
+# Input : [2, 1, 2]
+# Return  : 2 which occurs 2 times which is greater than 3/2. 
 
 ########################################################################################################################################
 
