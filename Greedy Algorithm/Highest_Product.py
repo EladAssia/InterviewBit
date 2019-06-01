32ec221ad4507d49c3f2527a61cd7eb0183db54c
EladAssia <assiaelad@gmail.com>
2019-06-01 12:20:12 +0300

    Update Highest_Product.py

--- Greedy Algorithm/Highest_Product.py
+++ Greedy Algorithm/Highest_Product.py
@@ -1,14 +1,15 @@
-Given an array of integers, return the highest product possible by multiplying 3 numbers from the array
+# Given an array of integers, return the highest product possible by multiplying 3 numbers from the array
 
-Input:
+# Input:
 
-array of integers e.g {1, 2, 3}
- NOTE: Solution will fit in a 32-bit signed integer 
-Example:
+# array of integers e.g {1, 2, 3}
+# NOTE: Solution will fit in a 32-bit signed integer 
 
-[0, -1, 3, 100, 70, 50]
+#  Example:
 
-=> 70*50*100 = 350000
+# [0, -1, 3, 100, 70, 50]
+
+# => 70*50*100 = 350000
 
 ########################################################################################################################################
 
