3050724b12757b80e645a007f8bb5092542d0551
EladAssia <assiaelad@gmail.com>
2019-06-01 12:03:17 +0300

    Create Best_Time_to_Buy_and_Sell_Stocks_I.py

---
+++ Dynamic Programming/Best_Time_to_Buy_and_Sell_Stocks_I.py
@@ -0,0 +1,31 @@
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example :
+
+Input : [1 2]
+Return :  1
+
+########################################################################################################################################
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxProfit(self, A):
+        if len(A) <= 1:
+            return 0
+        ans = 0
+        tmp_max, tmp_min = A[0], A[0]
+        
+        for ii in range(1, len(A)):
+            if A[ii] > tmp_max:
+                tmp_max = A[ii]
+            elif A[ii] < tmp_min:
+                ans = max(ans, tmp_max - tmp_min)
+                tmp_min, tmp_max = A[ii], A[ii]
+        
+        ans = max(ans, tmp_max - tmp_min)
+        return ans
+                
+########################################################################################################################################
