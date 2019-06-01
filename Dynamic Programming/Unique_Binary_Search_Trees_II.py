6f65260bccf5c9c6fdfdbfda2ecd97e58a213797
EladAssia <assiaelad@gmail.com>
2019-06-01 12:14:20 +0300

    Update Unique_Binary_Search_Trees_II.py

--- Dynamic Programming/Unique_Binary_Search_Trees_II.py
+++ Dynamic Programming/Unique_Binary_Search_Trees_II.py
@@ -1,16 +1,15 @@
-Given A, how many structurally unique BST’s (binary search trees) that store values 1...A?
+# Given A, how many structurally unique BST’s (binary search trees) that store values 1...A?
 
-Example :
+# Example :
 
-Given A = 3, there are a total of 5 unique BST’s.
+# Given A = 3, there are a total of 5 unique BST’s.
 
 
-   1         3     3      2      1
-    \       /     /      / \      \
-     3     2     1      1   3      2
-    /     /       \                 \
-   2     1         2                 3
-
+#    1         3     3      2      1
+#     \       /     /      / \      \
+#      3     2     1      1   3      2
+#     /     /       \                 \
+#    2     1         2                 3
 
 ########################################################################################################################################
 
