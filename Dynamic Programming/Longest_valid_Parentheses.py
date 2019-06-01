04adfa74b09fdc3bf83f8f8e275fd966dd57a594
EladAssia <assiaelad@gmail.com>
2019-06-01 12:00:54 +0300

    Create Longest_valid_Parentheses.py

---
+++ Dynamic Programming/Longest_valid_Parentheses.py
@@ -0,0 +1,31 @@
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+For "(()", the longest valid parentheses substring is "()", which has length = 2.
+
+Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
+
+########################################################################################################################################
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def longestValidParentheses(self, A):
+        if len(A) <= 1:
+            return 0
+
+        maxx = 0
+        lst = [-1]
+
+        for ii in range(len(A)):
+            if A[ii] == ')':
+                lst.pop()
+                if len(lst) != 0:
+                    maxx = max(maxx, ii - lst[len(lst) - 1])
+                else:
+                    lst.append(ii)
+            else:
+                lst.append(ii)
+
+        return maxx
+        
+########################################################################################################################################
