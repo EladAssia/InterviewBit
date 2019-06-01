d6f0bb027687fd37235a61674f749cbeb1e40d7d
EladAssia <assiaelad@gmail.com>
2019-06-01 12:13:27 +0300

    Update Sub_Matrices_with_sum_Zero.py

--- Dynamic Programming/Sub_Matrices_with_sum_Zero.py
+++ Dynamic Programming/Sub_Matrices_with_sum_Zero.py
@@ -1,24 +1,25 @@
-Given a 2D matrix, find the number non-empty sub matrices, such that the sum of the elements inside the sub matrix is equal to 0. (note: elements might be negative).
+# Given a 2D matrix, find the number non-empty sub matrices, such that the sum of the elements inside the sub matrix is equal to 0. 
+# (note: elements might be negative).
 
-Example:
+# Example:
 
-Input
+# Input
 
--8 5  7
-3  7 -8
-5 -8  9
+# -8 5  7
+# 3  7 -8
+# 5 -8  9
 
-Output
-2
+# Output
+# 2
 
-Explanation
--8 5 7
-3 7 -8
-5 -8 9
+# Explanation
+# -8 5 7
+# 3 7 -8
+# 5 -8 9
 
--8 5 7
-3 7 -8
-5 -8 9
+# -8 5 7
+# 3 7 -8
+# 5 -8 9
 
 ########################################################################################################################################
 
