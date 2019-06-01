6db763718cf44a722da826c20a35f9fba579d77d
EladAssia <assiaelad@gmail.com>
2019-06-01 12:10:25 +0300

    Update Length_of_Longest_Subsequence.py

--- Dynamic Programming/Length_of_Longest_Subsequence.py
+++ Dynamic Programming/Length_of_Longest_Subsequence.py
@@ -1,12 +1,12 @@
-Given an array of integers, find the length of longest subsequence which is first increasing then decreasing.
+# Given an array of integers, find the length of longest subsequence which is first increasing then decreasing.
 
-**Example: **
+# **Example: **
 
-For the given array [1 11 2 10 4 5 2 1]
+# For the given array [1 11 2 10 4 5 2 1]
 
-Longest subsequence is [1 2 10 4 2 1]
+# Longest subsequence is [1 2 10 4 2 1]
 
-Return value 6
+# Return value 6
 
 ########################################################################################################################################
 
