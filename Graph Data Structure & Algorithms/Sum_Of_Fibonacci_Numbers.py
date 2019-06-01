9198645af3e201827e40777073a77be86af565d5
EladAssia <assiaelad@gmail.com>
2019-06-01 12:30:22 +0300

    Update Sum_Of_Fibonacci_Numbers.py

--- Graph Data Structure & Algorithms/Sum_Of_Fibonacci_Numbers.py
+++ Graph Data Structure & Algorithms/Sum_Of_Fibonacci_Numbers.py
@@ -1,12 +1,12 @@
-How many minimum numbers from fibonacci series are required such that sum of numbers should be equal to a given Number N?
-Note : repetition of number is allowed.
+# How many minimum numbers from fibonacci series are required such that sum of numbers should be equal to a given Number N?
+# Note : repetition of number is allowed.
 
-Example:
+# Example:
 
-N = 4
-Fibonacci numbers : 1 1 2 3 5 .... so on
-here 2 + 2 = 4 
-so minimum numbers will be 2 
+# N = 4
+# Fibonacci numbers : 1 1 2 3 5 .... so on
+# here 2 + 2 = 4 
+# so minimum numbers will be 2 
 
 ########################################################################################################################################
 
